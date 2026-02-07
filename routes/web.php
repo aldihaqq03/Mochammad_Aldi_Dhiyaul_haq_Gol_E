2404eceec523e3170c087e8ffd308d286b3041bd
aldihaqq03 <mochaldidhiyaulhaq@gmail.com>
2026-02-07 11:34:43 +0700

    variable untuk kalkulator one rm dips
    ada berat badan beban dan juga reps

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+//calculator untuk menghidung one reps max untuk dips//
 Route::get('/', function () {
     return view('welcome');
+    $weight = request('weight', '');
+    $reps = request('reps', '');
+    $bodyweight = request('bodyweight', '');
+    $hasilORM = null;
 });
