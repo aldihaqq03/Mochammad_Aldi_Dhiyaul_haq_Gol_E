7db0a9592d2de2c06940f2be5ad8d0370c415fef
aldihaqq03 <mochaldidhiyaulhaq@gmail.com>
2026-02-07 12:17:12 +0700

    rumus menghitung one rm
    mengembalikan nilai ke view

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,21 @@
 use Illuminate\Support\Facades\Route;
 //calculator untuk menghidung one reps max untuk dips//
 Route::get('/', function () {
-    return view('welcome');
+    return view('hitung beban one reps max dips anda cung');
     $weight = request('weight', '');
     $reps = request('reps', '');
     $bodyweight = request('bodyweight', '');
     $hasilORM = null;
+    if ($weight && $reps && $bodyweight) {
+        $totalBeban = $bodyweight + $weight;
+        $hasilORM = $totalBeban * (1 + (0.0333 * $reps));
+
+    }
+    return view('hasil', [
+        'bodyweight' => $bodyweight,
+        'weight' => $weight,
+        'reps' => $reps,
+        'hasil one reps max' => $hasilORM
+    ]);
+
 });
